3ea78637f99125f14d8e63e170f03638172f5a22
jesse zenon <jesse.zenon@gmail.com>
2022-04-07 10:18:51 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(names, string) {
+  let answer = names.filter(
+    (name) => name.toUpperCase() === string.toUpperCase()
+  );
+  return answer;
+}
+
+function fuzzyMatch(driversNames, letters) {
+  let answer2 = driversNames.filter(
+    (driverName) => driverName.substring(0, 2) === letters
+  );
+  return answer2;
+}
+
+function matchName(driverObjects, string) {
+  let answer3 = driverObjects.filter(
+    (driverObject) => driverObject.name === string
+  );
+  return answer3;
+}
